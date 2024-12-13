e7a592abdbc5fdb9628bbd81cdf97dd28435923e
Tanzim vai <khandakertanzimmahtab@gmail.com>
2024-12-12 22:51:09 -0500

    first commit

---
+++ tradeflow-client-site/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2 className="text-center bg-red-600">Hello connected</h2>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
+}
+
+export default App;
